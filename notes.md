55c0bae94966a8f9b535176721409d643c8e180f
Douglas Fils <drfils@gmail.com>
2019-07-16 07:46:42 -0700

    Improvements to Tangram for ESIP Summer 2019

--- notes.md
+++ notes.md
@@ -1,3 +1,7 @@
  httpclient -f POST 127.0.0.1:8080/uploader  datagraph@./datagraphs/dataset-full-BAD.json-ld  shapegraph@./shapegraphs/googleRecommended.ttl
  httpclient -f POST 127.0.0.1:8080/uploader  datagraph@./datagraphs/dataset-minimal-BAD.json-ld  shapegraph@./shapegraphs/googleRecommended.ttl
  httpclient -f POST 127.0.0.1:8080/uploader  datagraph@./datagraphs/dataset-minimal-BAD.json-ld  shapegraph@./shapegraphs/googleRecommended.ttl
+
+ httpclient -f POST http://tangram.geodex.org/uploader  datagraph@./datagraphs/dataset-minimal.json-ld  shapegraph@./shapegraphs/googleRequired.ttl
+
+
--- src/tangram/app.py
+++ src/tangram/app.py
@@ -8,66 +8,47 @@ from rdflib import Graph, plugin
 from rdflib.serializer import Serializer
 
 app = Flask(__name__,
-            static_url_path='',
-            static_folder='web/static',
-            template_folder='web/templates')
-
-@app.route('/foo', methods=['POST'])
-def foo():
-    if not request.json:
-        abort(400)
-    print(request.json)
-    return json.dumps(request.json)
-
-
-@app.route('/bar', methods=['POST'])
-def bar():
-    if not request.form:
-        abort(400)
-    print(request.form)
-    return json.dumps(request.form["dg"])
+        static_url_path='',
+        static_folder='web/static',
+        template_folder='web/templates')
 
 @app.route('/uploader', methods = ['GET', 'POST'])
 def upload_file():
-       if request.method == 'POST':
-                 dg  = request.files['datagraph']
-                 sg  = request.files['shapegraph']
-                 s = rdflib.Graph()
-                 sr = s.parse(sg, format="ttl")
-                 d = rdflib.Graph()
-                 dr = d.parse(dg, format="json-ld")
+        if request.method == 'POST':
+            dg  = request.files['datagraph']   # should try these too then error if not loadable to graph 
+            sg  = request.files['shapegraph']
+            try: 
+                f = request.form['format']
+            except: 
+                f = 'robot'
 
-                 conforms, v_graph, v_text = validate(dr, shacl_graph=sr,
-                                                      data_graph_format="json-ld",
-                                                      shacl_graph_format="ttl",
-                                                      inference='none', debug=False,
-                                                      serialize_report_graph=False)
+            # Make some graphs and parse our uploads into them
+            # How does python treat errors here?   trapped by Flask?  (see note about try above)
+            s = rdflib.Graph()
+            sr = s.parse(sg, format="ttl")
+            d = rdflib.Graph()
+            dr = d.parse(dg, format="json-ld")
 
-                 return  '{} {} {}'.format(conforms, v_graph, v_text)
+            # call pySHACL
+            conforms, v_graph, v_text = validate(dr, shacl_graph=sr,
+                data_graph_format="json-ld",
+                shacl_graph_format="ttl",
+                inference='none', debug=False,
+                serialize_report_graph=False)
 
+            # I default to robot, but then never both to test that  :(  
+            if f == 'human':
+                return  '{} {}'.format(conforms, v_text)
+            else:
+                return  v_graph.serialize()
 
-# TODO
-# add in static file sending
-# https://stackoverflow.com/questions/20646822/how-to-serve-static-files-in-flask
-
-@app.route('/data', methods=['POST'])
-def f_data():
-    if request.method == "POST":
-        fields = [k for k in request.form]
-        values = [request.form[k] for k in request.form]
-        data = dict(zip(fields, values))
-        print(request.form["dg"])
-        print(request.form["sg"])
-    # r = validate(data_graph, shacl_graph=sg, ont_graph=og, inference='rdfs', abort_on_error=False, meta_shacl=False, debug=False)
-    # conforms, results_graph, results_text = r
-    return jsonify(data)
 
 #@app.route('/')
 #def hello_world():
 #        return 'Tangram services are described at the GitHub Repo'
 @app.route("/")
 def index():
-       return render_template("index.html")
+        return render_template("index.html")
 
 if __name__ == "__main__":
         app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
